256615b2fadac8be3e971af4219fecd7ffbb506b
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-17 22:20:53 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,26 +18,24 @@ class LoginForm extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-
     if (!this.state.username || !this.state.password) return
-
     this.props.handleLogin(this.state)
     };
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} />
           </label>
         </div>
         <div>
